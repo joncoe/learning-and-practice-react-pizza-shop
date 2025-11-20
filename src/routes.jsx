101755dafb25823f535b5af2fd9243e042085629
Jonathan Coe <info@joncoe.com>
2025-11-20 10:50:41 +0900

    Add data loader to Order/:id route

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import Order from './features/order/Order';
+import Order, { loader as orderLoader } from './features/order/Order';
 import CreateOrder from './features/order/CreateOrder';
 import AppLayout from './ui/AppLayout';
 import Error from './ui/Error';
@@ -33,6 +33,8 @@ export const router = createBrowserRouter([
 			{
 				path: '/order/:orderId',
 				element: <Order />,
+				loader: orderLoader,
+				errorElement: <Error />,
 			},
 		],
 	},
